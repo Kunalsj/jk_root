da6114c96afde1c2048b4230bdebfb641eac1c6a
jerryaujla1 <jerryaujla1@gmail.com>
2017-05-31 01:11:08 -0700

    Created basic angularapp with example directive, need to refactor.

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer
 
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+
+    return render(request, 'base.html', {})
--- jk/settings.py
+++ jk/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'authentication'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'jk.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['src/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+STATICFILES_DIRS = ['app']
